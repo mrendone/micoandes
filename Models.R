b406e467abd831d9c2bc4e1b6f96bfd556034498
mrendone <mrendon1998@gmail.com>
2024-04-26 09:42:17 -0500

    Correciones y solución de conflictos

--- Models.R
+++ Models.R
@@ -2,7 +2,7 @@
 #    Models                     #
 #################################
 
-
+library(Metrics)
 library(readxl)
 Data10kmFiltrado13_12_20 <-read_excel("insumos/Data10kmFiltrado13_12_20.xlsx")
 
@@ -12,6 +12,8 @@ mayores_a_100m<-Data10kmFiltrado13_12_20 %>% filter(Elev10kmcell>100)
 
 mayores_a_100m<-Data10kmFiltrado13_12_20 %>% filter(Elev10kmcell>100)
 
+Data10kmFiltrado13_12_20<- Data10kmFiltrado13_12_20 %>% rename(id="IDceldas10km.IDcelda10km")
+Data10kmFiltrado_101 <-Data10kmFiltrado13_12_20%>%filter(Elev10kmcell>101)
 
 mayores_a_100m<-mutate(mayores_a_100m, Cuadrado_BIO1=(BIO1)**2)
 mayores_a_100m<-mutate(mayores_a_100m, Cuadrado_BIO12=(BIO12)**2)
@@ -20,6 +22,11 @@ mayores_a_100m<-mutate(mayores_a_100m, Cuadrado_PHIHOX=(PHIHOX)**2)
 mayores_a_100m<-mutate(mayores_a_100m, Cuadrado_Nitrogeno=(Nitrogeno)**2)
 mayores_a_100m<-mutate(mayores_a_100m, Cuadrado_OCSTHA=(OCSTHA)**2)
 
+
+
+Data10kmFiltrado_101$BIO12_log <- log(Data10kmFiltrado_101$BIO12)
+Data10kmFiltrado_101$PHIHOX_log <- log(Data10kmFiltrado_101$PHIHOX)
+Data10kmFiltrado_101$Nitrogeno_log <- log(Data10kmFiltrado_101$Nitrogeno)
 
 # Load necessary libraries
 library(gtsummary)
@@ -102,6 +109,16 @@ Data10kmFiltrado_101$AM_ratiocell <- replace(Data10kmFiltrado_101$AM_ratiocell,
 Modelo_final_AM_ratiocellNitrogenoGLM_AIC <- glm(AM_ratiocell ~ BIO1 + BIO12 + Nitrogeno,
                                                  data = Data10kmFiltrado_101, 
                                                  family = gaussian(link = "identity")) 
+
+
+Modelo_final_AM_ratiocellCarbonoGLM<-glm(AM_ratiocell~ BIO1 + BIO12 + OCSTHA,
+                                         data = Data10kmFiltrado_101, family=gaussian(link = "identity"))
+
+
+Modelo_final_AM_ratiocellCarbonoZIB<-glmmTMB(AM_ratiocell ~ BIO1 + BIO12_log  + OCSTHA,
+                                             ziformula=~1, 
+                                             family=beta_family(), 
+                                             data = Data10kmFiltrado_101)
 
 # Boxplots to visualize outliers
 boxplot(Data10kmFiltrado_101$BIO1, main = "BIO1")
@@ -252,6 +269,18 @@ Modelo_Final_OM_ratiocell_NitrogenoZIB <- glmmTMB(OM_ratiocell ~ BIO1 + BIO12_lo
 
 
 
+
+
+
+Modelo_Final_OM_ratiocell_CarbonoGLM<-glm(OM_ratiocell~ BIO1 + BIO12 + OCSTHA,
+                                          data = Data10kmFiltrado_101, family=gaussian(link = "identity")) 
+
+
+Modelo_Final_OM_ratiocell_CarbonoZIB<- glmmTMB(OM_ratiocell ~ BIO1 + BIO12_log + OCSTHA,
+                                               ziformula=~1, 
+                                               family=beta_family(), 
+                                               data = Data10kmFiltrado_101)
+
 aic_OM_ratiocell_CarbonoGLM<- AIC(Modelo_Final_OM_ratiocell_CarbonoGLM)
 aic_OM_ratiocell_CarbonoZIB <- AIC(Modelo_Final_OM_ratiocell_CarbonoZIB)
 
@@ -259,5 +288,183 @@ print(paste("AIC OM_ratiocell Carbono GLM: ", aic_OM_ratiocell_CarbonoGLM))
 print(paste("AIC OM_ratiocell Carbono ZIB: ", aic_OM_ratiocell_CarbonoZIB))
 
 
+Modelo_Final_WanNm_ratiocell_NitrogenoGLM<-glm(WanNm_ratiocell~ BIO1 + BIO12 + Nitrogeno,
+                                               data = Data10kmFiltrado_101, family=gaussian(link = "identity")) 
 
 
+
+Modelo_Final_WanNm_ratiocell_NitrogenoZIB<- glmmTMB(WanNm_ratiocell ~ BIO1 + BIO12_log + Nitrogeno_log,
+                                                    ziformula=~1, 
+                                                    family=beta_family(), 
+                                                    data = Data10kmFiltrado_101)
+
+aic_WanNm_ratiocell_NitrogenoGLM<- AIC(Modelo_Final_WanNm_ratiocell_NitrogenoGLM)
+aic_WanNm_ratiocelll_NitrogenoZIB <- AIC(Modelo_Final_WanNm_ratiocell_NitrogenoZIB)
+
+print(paste("AIC ErM_ratiocell Nitrogeno GLM: ", aic_WanNm_ratiocell_NitrogenoGLM))
+print(paste("AIC ErM_ratiocell Nitrogeno ZIB: ", aic_WanNm_ratiocelll_NitrogenoZIB))
+
+Modelo_Final_WanNm_ratiocell_CarbonoGLM<-glm(WanNm_ratiocell~ BIO1 + BIO12 + OCSTHA,
+                                             data = Data10kmFiltrado_101, family=gaussian(link = "identity")) 
+
+
+
+Modelo_Final_WanNm_ratiocell_CarbonoZIB<- glmmTMB(WanNm_ratiocell ~ BIO1 + BIO12_log + OCSTHA,
+                                                  ziformula=~1, 
+                                                  family=beta_family(), 
+                                                  data = Data10kmFiltrado_101)
+
+
+
+aic_WanNm_ratiocell_CarbonoGLM<- AIC(Modelo_Final_WanNm_ratiocell_CarbonoGLM)
+aic_WanNm_ratiocell_CarbonoZIB <- AIC(Modelo_Final_WanNm_ratiocell_CarbonoZIB)
+
+print(paste("AIC WanNm_ratiocell Carbono GLM: ", aic_WanNm_ratiocell_CarbonoGLM))
+print(paste("AIC WanNm_ratiocell Carbono ZIB: ", aic_WanNm_ratiocell_CarbonoZIB))
+
+
+Modelo_Final_Nfix_ratiocell_NitrogenoGLM<-glm(Nfix_ratiocell~ BIO1 + BIO12 + Nitrogeno,
+                                              data = Data10kmFiltrado_101, family=gaussian(link = "identity")) 
+
+
+
+Modelo_Final_Nfix_ratiocell_NitrogenoZIB<- glmmTMB(Nfix_ratiocell ~ BIO1 + BIO12_log + Nitrogeno_log,
+                                                   ziformula=~1, 
+                                                   family=beta_family(), 
+                                                   data = Data10kmFiltrado_101)
+
+aic_Nfix_ratiocell_NitrogenooGLM<- AIC(Modelo_Final_Nfix_ratiocell_NitrogenoGLM)
+aic_Nfix_ratiocell_NitrogenoZIB <- AIC(Modelo_Final_Nfix_ratiocell_NitrogenoZIB)
+
+print(paste("AIC Nfix_ratiocell Nitrogeno GLM: ", aic_Nfix_ratiocell_NitrogenooGLM))
+print(paste("AIC Nfix_ratiocell Nitrogeno ZIB: ", aic_Nfix_ratiocell_NitrogenoZIB))
+
+Modelo_Final_Nfix_ratiocell_OCSTHAGLM<-glm(Nfix_ratiocell~ BIO1 + BIO12 + OCSTHA,
+                                           data = Data10kmFiltrado_101, family=gaussian(link = "identity")) 
+
+
+
+Modelo_Final_Nfix_ratiocell_OCSTHAZIB<- glmmTMB(Nfix_ratiocell ~ BIO1 + BIO12_log + OCSTHA,
+                                                ziformula=~1, 
+                                                family=beta_family(), 
+                                                data = Data10kmFiltrado_101)
+
+aic_Nfix_ratiocell_CarbonoGLM<- AIC(Modelo_Final_Nfix_ratiocell_OCSTHAGLM)
+aic_Nfix_ratiocell_CarbonoZIB <- AIC(Modelo_Final_Nfix_ratiocell_OCSTHAZIB)
+
+print(paste("AIC Nfix_ratiocell Carbono GLM: ", aic_Nfix_ratiocell_CarbonoGLM))
+print(paste("AIC Nfix_ratiocell Carbono ZIB: ", aic_Nfix_ratiocell_CarbonoZIB))
+
+#Effect size proporción micorrizas
+
+library(QuantPsyc)
+
+#Nfix
+coefficients_NfixCarbono<-lm.beta(Modelo_Final_Nfix_ratiocell_OCSTHAGLM)
+coefficients_NfixNitro<-lm.beta(Modelo_Final_Nfix_ratiocell_NitrogenoGLM)
+
+#AM
+coefficients_AMCarbono<-lm.beta(Modelo_final_AM_ratiocellCarbonoGLM)
+coefficients_AMNitrogeno<-lm.beta(Modelo_final_AM_ratiocellNitrogenoGLM_AIC)
+
+#EM
+coefficients_EMNitrogeno<-lm.beta(Modelo_Final_EM_ratiocell_NitrogenoGLM)
+coefficients_EMCarbono<-lm.beta(Modelo_final_EM_ratiocell_CarbonoGLM)
+
+
+#ErM
+coefficients_ErMNitrogeno<-lm.beta(Modelo_Final_ErM_ratiocell_NitrogenoGLM)
+coefficients_ErMCarbono<-lm.beta(Modelo_Final_ErM_ratiocell_CarbonoGLM)
+
+#OM
+coefficients_OMNitrogeno<-lm.beta(Modelo_Final_OM_ratiocell_NitrogenoGLM)
+coefficients_OMCarbono<-lm.beta(Modelo_Final_OM_ratiocell_CarbonoGLM)
+
+#WamNm
+
+coefficients_WanNmNitrogeno<-lm.beta(Modelo_Final_WanNm_ratiocell_NitrogenoGLM)
+coefficients_WanNmCarbono<-lm.beta(Modelo_Final_WanNm_ratiocell_CarbonoGLM)
+
+
+print(coefficients_WanNmNitrogeno)
+  
+library(MuMIn)
+library(Metrics)
+
+Modelos_Nitrogeno<-list(Modelo_final_AM_ratiocellNitrogenoGLM_AIC,Modelo_final_AM_ratiocellNitrogenoZIB_AIC,
+                        Modelo_Final_EM_ratiocell_NitrogenoGLM,Modelo_Final_EM_ratiocell_NitrogenoZIB,
+                        Modelo_Final_ErM_ratiocell_NitrogenoGLM, Modelo_Final_ErM_ratiocell_NitrogenoZIB,
+                        Modelo_Final_Nfix_ratiocell_NitrogenoGLM,Modelo_Final_Nfix_ratiocell_NitrogenoZIB,
+                        Modelo_Final_OM_ratiocell_NitrogenoGLM,Modelo_Final_OM_ratiocell_NitrogenoZIB,
+                        Modelo_Final_WanNm_ratiocell_NitrogenoGLM, Modelo_Final_WanNm_ratiocell_NitrogenoZIB)
+
+Modelos_Nitrogeno_<-c("Modelo_final_AM_ratiocellNitrogenoLM_AIC",
+                      "Modelo_final_AM_ratiocellNitrogenoZIB_AIC",
+                      "Modelo_Final_EM_ratiocell_NitrogenoLM","Modelo_Final_EM_ratiocell_NitrogenoZIB",
+                      "Modelo_Final_ErM_ratiocell_NitrogenoLM", "Modelo_Final_ErM_ratiocell_NitrogenoZIB",
+                      "Modelo_Final_Nfix_ratiocell_NitrogenoLM","Modelo_Final_Nfix_ratiocell_NitrogenoZIB",
+                      "Modelo_Final_OM_ratiocell_NitrogenoLM","Modelo_Final_OM_ratiocell_NitrogenoZIB",
+                      "Modelo_Final_WanNm_ratiocell_NitrogenoLM","Modelo_Final_WanNm_ratiocell_NitrogenoZIB")
+
+Modelos_OCSTHA<-list(Modelo_final_AM_ratiocellCarbonoGLM,Modelo_final_AM_ratiocellCarbonoZIB,
+                     Modelo_final_EM_ratiocell_CarbonoGLM,
+                     Modelo_final_EM_ratiocell_CarbonoZIB,
+                     Modelo_Final_ErM_ratiocell_CarbonoGLM, Modelo_Final_ErM_ratiocell_CarbonoZIB,
+                     Modelo_Final_Nfix_ratiocell_OCSTHAGLM,Modelo_Final_Nfix_ratiocell_OCSTHAZIB,
+                     Modelo_Final_OM_ratiocell_CarbonoGLM,Modelo_Final_OM_ratiocell_CarbonoZIB,
+                     Modelo_Final_WanNm_ratiocell_CarbonoGLM, Modelo_Final_WanNm_ratiocell_CarbonoZIB)
+
+
+Modelos_Carbono<-c("Modelo_final_AM_ratiocellCarbonoLM","Modelo_final_AM_ratiocellCarbonoZIB",
+                   "Modelo_final_EM_ratiocell_CarbonoLM",
+                   "Modelo_final_EM_ratiocell_CarbonoZIB",
+                   "Modelo_Final_ErM_ratiocell_CarbonoLM", "Modelo_Final_ErM_ratiocell_CarbonoZIB",
+                   "Modelo_Final_Nfix_ratiocell_OCSTHALM","Modelo_Final_Nfix_ratiocell_OCSTHAZIB",
+                   "Modelo_Final_OM_ratiocell_CarbonoLM","Modelo_Final_OM_ratiocell_CarbonoZIB",
+                   "Modelo_Final_WanNm_ratiocell_CarbonoLM", "Modelo_Final_WanNm_ratiocell_CarbonoZIB")
+
+AIC_Nitrogeno<-lapply(Modelos_Nitrogeno, FUN = AIC)
+
+#Revisar 
+
+
+Rmse_Nitrogeno<-c(rmse(Data10kmFiltrado_101$AM_ratiocell,predict(Modelo_final_AM_ratiocellNitrogenoGLM_AIC)),
+                  rmse(Data10kmFiltrado_101$AM_ratiocell,predict(Modelo_final_AM_ratiocellNitrogenoZIB_AIC)),
+                  rmse(Data10kmFiltrado_101$EM_ratiocell,predict(Modelo_Final_EM_ratiocell_NitrogenoGLM)),
+                  rmse(Data10kmFiltrado_101$EM_ratiocell,predict(Modelo_Final_EM_ratiocell_NitrogenoZIB)),
+                  rmse(Data10kmFiltrado_101$ErM_ratiocell,predict(Modelo_Final_ErM_ratiocell_NitrogenoGLM)),
+                  rmse(Data10kmFiltrado_101$ErM_ratiocell,predict(Modelo_Final_ErM_ratiocell_NitrogenoZIB)),
+                  rmse(Data10kmFiltrado_101$Nfix_ratiocell,predict(Modelo_Final_Nfix_ratiocell_NitrogenoGLM)),
+                  rmse(Data10kmFiltrado_101$Nfix_ratiocell,predict(Modelo_Final_Nfix_ratiocell_NitrogenoZIB)),
+                  rmse(Data10kmFiltrado_101$OM_ratiocell,predict(Modelo_Final_OM_ratiocell_NitrogenoGLM)),
+                  rmse(Data10kmFiltrado_101$OM_ratiocell,predict(Modelo_Final_OM_ratiocell_NitrogenoZIB)),
+                  rmse(Data10kmFiltrado_101$WanNm_ratiocell,predict(Modelo_Final_WanNm_ratiocell_NitrogenoGLM)),
+                  rmse(Data10kmFiltrado_101$WanNm_ratiocell,predict(Modelo_Final_WanNm_ratiocell_NitrogenoZIB)))
+
+Rmse_Carbono<-c(rmse(Data10kmFiltrado_101$AM_ratiocell,
+                     predict(Modelo_final_AM_ratiocellCarbonoGLM)),
+                rmse(Data10kmFiltrado_101$AM_ratiocell,predict(Modelo_final_AM_ratiocellCarbonoZIB)),
+                rmse(Data10kmFiltrado_101$EM_ratiocell,predict(Modelo_final_EM_ratiocell_CarbonoGLM)),
+                rmse(Data10kmFiltrado_101$EM_ratiocell,predict(Modelo_final_EM_ratiocell_CarbonoZIB)),
+                rmse(Data10kmFiltrado_101$ErM_ratiocell,predict(Modelo_Final_ErM_ratiocell_CarbonoGLM)),
+                rmse(Data10kmFiltrado_101$ErM_ratiocell,predict(Modelo_Final_ErM_ratiocell_CarbonoZIB)),
+                rmse(Data10kmFiltrado_101$Nfix_ratiocell,predict(Modelo_Final_Nfix_ratiocell_OCSTHAGLM)),
+                rmse(Data10kmFiltrado_101$Nfix_ratiocell,predict(Modelo_Final_Nfix_ratiocell_OCSTHAZIB)),
+                rmse(Data10kmFiltrado_101$OM_ratiocell,predict(Modelo_Final_OM_ratiocell_CarbonoGLM)),
+                rmse(Data10kmFiltrado_101$OM_ratiocell,predict(Modelo_Final_OM_ratiocell_CarbonoZIB)),
+                rmse(Data10kmFiltrado_101$WanNm_ratiocell,predict(Modelo_Final_WanNm_ratiocell_CarbonoGLM)),
+                rmse(Data10kmFiltrado_101$WanNm_ratiocell,predict(Modelo_Final_WanNm_ratiocell_CarbonoZIB)))
+
+
+
+
+
+
+
+
+resultados_Nitrogeno<-cbind(Modelos_Nitrogeno_,Rmse_Nitrogeno)
+
+resultados_Carbono<-cbind(Modelos_Carbono, Rmse_Carbono)
+
+View(resultados_Nitrogeno)
+View(resultados_Carbono)
